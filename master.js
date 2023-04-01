a4f3e564d8617ea3534e54d1e65652c19fa8782e
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-01 10:08:36 -0500

    Implementation of differential equations complete. Gravity is in the wrong direction for some reason

--- master.js
+++ master.js
@@ -1,12 +1,14 @@
+const { cos, sin } = Math;
+
 let WIDTH,
     HEIGHT,
     weight1,
     weight2,
-    angle1 = Math.PI / 4,
-    angle2 = Math.PI / 2,
-    angle1V = 0.01,
+    angle1 = PI / 4,
+    angle2 = PI / 2,
+    angle1V = 0,
     angle1A = 0,
-    angle2V = 0.01,
+    angle2V = 0,
     angle2A = 0;
 
 const canvas = document.getElementById("canvas"),
@@ -20,6 +22,8 @@ const canvas = document.getElementById("canvas"),
     PADDING_TOP = 200,
     gravity = 3,
     dampening = 0.9975;
+
+const square = num => Math.pow(num, 2);
 
 const requestAnimationFrame =
     window.requestAnimationFrame ||
@@ -37,10 +41,6 @@ function init() {
     background.setAttribute("height", HEIGHT);
 
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    ctx.closePath();
 
     const pos1 = Vector.fromAngle(angle1).setMag(LINE_1);
 
@@ -58,13 +58,17 @@ function init() {
         c: "#f88"
     });
 
+    backgroundCtx.translate(WIDTH / 2, PADDING_TOP);
+    backgroundCtx.lineWidth = 5;
+    backgroundCtx.strokeStyle = "rgba(180, 255, 180, 0.5)";
+    backgroundCtx.moveTo(weight2.pos.x, weight2.pos.y);
+
     ani();
 }
 
 function ani() {
     ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.closePath();
 
     ctx.save();
@@ -81,14 +85,47 @@ function ani() {
     weight2.show();
     ctx.restore();
 
-    angle1A = (Math.cos(angle1) * gravity) / LINE_1;
+    backgroundCtx.lineTo(weight2.pos.x, weight2.pos.y);
+    backgroundCtx.clearRect(-WIDTH / 2, -PADDING_TOP, WIDTH * 1.5, HEIGHT + PADDING_TOP);
+    backgroundCtx.stroke();
+
+    // angular acceleration calculations
+    // TODO: credit myphisicslab.com's article
+    let num1, num2, num3, num4, den;
+
+    // Angle1
+    num1 = gravity * (2 * MASS_1 + MASS_2) * sin(angle1);
+    num2 = MASS_2 * gravity * sin(angle1 - 2 * angle2);
+    num3 =
+        2 *
+        sin(angle1 - angle2) *
+        MASS_2 *
+        (square(angle2V) * LINE_2 + square(angle1V) * LINE_1 * cos(angle1 - angle2));
+    den = LINE_1 * (2 * MASS_1 + MASS_2 - MASS_2 * cos(2 * angle1 - 2 * angle2));
+
+    angle1A = (-num1 - num2 - num3) / den;
+
+    // Angle2
+    num1 = 2 * sin(angle1 - angle2);
+    num2 = square(angle1V) * LINE_1 * (MASS_1 + MASS_2);
+    num3 = gravity * (MASS_1 + MASS_2) * cos(angle1);
+    num4 = square(angle2V) * LINE_2 * MASS_2 * cos(angle1, angle2);
+    den = LINE_2 * (2 * MASS_1 + MASS_2 - MASS_2 * cos(2 * angle1 - 2 * angle2));
+
+    angle2A = (num1 * (num2 + num3 + num4)) / den;
+
     angle1V += angle1A;
     angle1V *= dampening;
     angle1 += angle1V;
 
-    weight1.pos = Vector.fromAngle(angle1).setMag(LINE_1);
+    angle2V += angle2A;
+    angle2V *= dampening;
+    angle2 += angle2V;
 
-    // requestAnimationFrame(ani);
+    weight1.pos = Vector.fromAngle(angle1).setMag(LINE_1);
+    weight2.pos = Vector.add(weight1.pos, Vector.fromAngle(angle2).setMag(LINE_2));
+
+    requestAnimationFrame(ani);
 }
 
 init();
